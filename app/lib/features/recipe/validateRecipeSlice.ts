8d1e038e2b84c8308bc94dd7673e1ee471ddbfac
Kevin Ashline <kevinashline@Kevins-MacBook-Pro.local>
2024-05-11 15:49:36 -0700

    removed file blocking deployment

--- app/lib/features/recipe/validateRecipeSlice.ts
+++
@@ -1,9 +0,0 @@
-import { useAppDispatch, useAppSelector } from "../../hooks"
-import { selectCreateRecipe, setNameValid } from "./createRecipeSlice"
-
-const createRecipe = useAppSelector(selectCreateRecipe)
-const dispatch = useAppDispatch()
-
-export const validateName = () => {
-    dispatch(setNameValid(false))
-}
