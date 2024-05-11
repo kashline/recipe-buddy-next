9d1fe2bf23f27a4dbef58b859770e104e74a7e12
Kevin Ashline <kevinashline@Kevins-MacBook-Pro.local>
2024-05-11 15:51:35 -0700

    removed file blocking deployment

--- app/recipes/create/validateRecipe.ts
+++
@@ -1,6 +0,0 @@
-import { selectCreateRecipe } from "@/app/lib/features/recipe/createRecipeSlice";
-import { useAppSelector } from "@/app/lib/hooks";
-
-export default function validateRecipe(){
-    useAppSelector(selectCreateRecipe)
-}
