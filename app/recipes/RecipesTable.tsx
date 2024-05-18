f3f5d0dc7ee3427a9da06b41d627bd14721d4a79
Kevin Ashline <kevinashline@Kevins-MacBook-Pro.local>
2024-05-18 15:06:59 -0700

    nostore on table seems to be required..

--- app/recipes/RecipesTable.tsx
+++ app/recipes/RecipesTable.tsx
@@ -11,6 +11,7 @@ export default async function RecipesTable({
   query: string;
 }) {
   try{
+    noStore()
     const recipeData: Map<string, Object> = new Map(await (await fetch(`${process.env.APP_URL}/api/recipes${query}`)).json())
     const recipes: any = recipeData.get('recipes')
     const recipesPerPage = 10
