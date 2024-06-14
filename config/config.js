fd1834fcb87cf56d0245ce7c0f2044d0d7734c07
Kevin Ashline <kevinashline@Kevins-MacBook-Pro.local>
2024-06-14 10:56:39 -0700

    fixing migration's db connection in prod

---
+++ config/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "development": {
+      "username": "postgres",
+      "password": "uYOP9g2XtF",
+      "database": "kevinashline",
+      "host": "localhost",
+      "dialect": "postgres"
+    },
+    "production": {
+      "username": process.env.POSTGRES_USER,
+      "password": process.env.POSTGRES_PASSWORD,
+      "database": process.env.POSTGRES_DATABASE,
+      "host": process.env.POSTGRES_HOST,
+      "port": 5432,
+      "dialect": "postgres"
+    }
+}
+
