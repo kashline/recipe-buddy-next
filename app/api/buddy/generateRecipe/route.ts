be8621e07efbc04942f2cf3bfea39f97ff4d1f1b
Kevin Ashline <kevinashline@Kevins-MacBook-Pro.local>
2024-09-11 11:08:27 -0700

    removed old comment and imports

--- app/api/buddy/generateRecipe/route.ts
+++ app/api/buddy/generateRecipe/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import { generateObject } from "ai";
 import { openai } from '@ai-sdk/openai';
-import { HttpResponseOutputParser } from "langchain/output_parsers";
 import { RecipeZodel } from "@/app/lib/data/zodels/Recipe";
 
 export const runtime = "edge";
@@ -17,8 +16,6 @@ export const POST = async (req: Request) => {
         schema: RecipeZodel,
         prompt: currentMessageContent
     })
-    const outputParser = new HttpResponseOutputParser();
-    // Not sure what's happening here, streamText doesn't appear to have toDataStreamResponse function on it.  The deprecated StreamingTextReponse is currently working.
     return new Response(JSON.stringify(object), {
         status: 200,
         })
