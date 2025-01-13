214b7f53af69427f65bf913e039c93de71dec2ce
Kevin Ashline <kevin.ashline92@gmail.com>
2025-01-13 10:47:56 -0800

    cleanup and import page skeleton

---
+++ app/recipes/import/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import * as React from "react";
+
+export default function Page() {
+  const [url, setUrl] = React.useState<string>("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(e.target.value);
+    console.log(url);
+  };
+  React.useEffect(() => {
+    console.log(url);
+  });
+  return (
+    <div>
+      <div className="flex">
+        <label className="text-lavendar-blush pr-2">Import URL:</label>
+        <input className="h-6" name="importUrl" onChange={handleChange} />
+      </div>
+      <button className="shadow-white">Submit</button>
+    </div>
+  );
+}
